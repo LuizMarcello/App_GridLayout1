f9529720fb87dd9c041495da1edd308ecb086a12
luiz marcello <luizmarcello.vmo@hotmail.com>
2020-06-18 11:09:23 -0300

    18.06.2020_11:09hs

--- App_GridLayout1/App_GridLayout1/App_GridLayout1/GridPage1.xaml.cs
+++ App_GridLayout1/App_GridLayout1/App_GridLayout1/GridPage1.xaml.cs
@@ -15,6 +15,25 @@ namespace App_GridLayout1
         public GridPage1()
         {
             InitializeComponent();
+
+            var grid = new Grid
+            {
+                BackgroundColor = Color.Yellow,
+                RowSpacing = 20,
+                ColumnSpacing = 20,
+            };
+
+            var label1 = new Label { Text = "Label1", BackgroundColor = Color.Aqua };
+            var label2 = new Label { Text = "Label2", BackgroundColor = Color.Aqua };
+            var label3 = new Label { Text = "Label3", BackgroundColor = Color.Aqua };
+            var label4 = new Label { Text = "Label4", BackgroundColor = Color.Aqua };
+
+            grid.Children.Add(label1, 0, 0);
+            grid.Children.Add(label2, 0, 1);
+            grid.Children.Add(label3, 1, 0);
+            grid.Children.Add(label4, 1, 1);
+
+            Content = grid;
         }
     }
 }
